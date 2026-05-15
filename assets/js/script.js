ef5f7b5c3ee72d063b2dfba2fd88005704970b6a
Darun-555 <dka4000@hw.ac.uk>
2026-05-15 14:22:21 +0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
-console.log("Portfolio loaded successfully.");
+const filterButtons = document.querySelectorAll(".filter-btn");
+const projectCards = document.querySelectorAll(".filter-item");
+
+filterButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const selectedFilter = button.getAttribute("data-filter");
+
+    filterButtons.forEach((btn) => btn.classList.remove("active"));
+    button.classList.add("active");
+
+    projectCards.forEach((card) => {
+      const categories = card.getAttribute("data-category");
+
+      if (selectedFilter === "all" || categories.includes(selectedFilter)) {
+        card.classList.remove("hide");
+      } else {
+        card.classList.add("hide");
+      }
+    });
+  });
+});
